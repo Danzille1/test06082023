7f9204fcc878faaf2141070273a4abb984529ff1
Danzille1 <chopper9751@mail.ru>
2023-08-06 23:57:33 +0500

    Sem 2 Task 15

---
+++ Examples/Example9/Program.cs
@@ -0,0 +1,23 @@
+﻿int n1 = new Random().Next(1,8);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+
+if (n1 == 6)
+ {
+  Console.Write(n1);     
+  Console.WriteLine(" -> да");
+ }
+ else
+ {
+  if (n1 == 7)
+  {
+   Console.Write(n1);     
+   Console.WriteLine(" -> да");
+  }
+  else
+  { 
+   Console.Write(n1);     
+   Console.WriteLine(" -> нет");
+  }
+ }
