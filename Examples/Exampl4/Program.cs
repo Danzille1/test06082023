794d7b6c0f9d44dd8c45f475a311ebb74821c92f
Danzille1 <chopper9751@mail.ru>
2023-08-06 20:52:42 +0500

    Sem 1 Task 4

---
+++ Examples/Exampl4/Program.cs
@@ -0,0 +1,35 @@
+﻿int n1 = new Random().Next(1,100);
+int n2 = new Random().Next(1,100);
+int n3 = new Random().Next(1,100);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+
+Console.Write("n2= ");
+Console.WriteLine(n2);
+
+Console.Write("n3= ");
+Console.WriteLine(n3);
+
+if (n1 > n2) 
+ {
+  if (n1 > n3) 
+   {
+    Console.WriteLine("n1 - максимальное число");
+   }
+   else
+    {
+     Console.WriteLine("n3 - максимальное число");
+    }
+ }
+  else
+ { 
+  if (n2 > n3) 
+   {
+    Console.WriteLine("n2 - максимальное число"); 
+   } 
+    else
+    {
+     Console.WriteLine("n3 - максимальное число");    
+    } 
+ }
