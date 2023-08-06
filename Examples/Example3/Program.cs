16a36e68ce2882cd585506152a2a0d3a7c14eed2
Danzille1 <chopper9751@mail.ru>
2023-08-06 20:45:56 +0500

    Sem1 Task 2

---
+++ Examples/Example3/Program.cs
@@ -0,0 +1,16 @@
+﻿int n1 = new Random().Next(1,100);
+int n2 = new Random().Next(1,100);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+Console.Write("n2= ");
+Console.WriteLine(n2);
+
+if (n1 > n2) 
+ {
+  Console.WriteLine("n1 больше чем n2");
+ }
+ else
+ { 
+  Console.WriteLine("n2 больше чем n1");
+ }
