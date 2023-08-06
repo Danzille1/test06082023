d28972849e9dc4bd109e7ddb75836aafb613c3ec
Danzille1 <chopper9751@mail.ru>
2023-08-06 21:09:58 +0500

    Sem 1 Task 8

---
+++ Examples/Example6/Program.cs
@@ -0,0 +1,14 @@
+﻿int n1 = new Random().Next(1,100);
+int index = 1;
+
+while (index <= n1)
+{
+ if (index % 2 == 0) 
+  {
+   Console.WriteLine(index);
+  }
+ index++; 
+}
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
