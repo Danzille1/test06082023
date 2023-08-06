fb6f65ee2aa77a444908679085ab474088789a46
Danzille1 <chopper9751@mail.ru>
2023-08-06 21:02:37 +0500

    Sem 1 Task 6

---
+++ Examples/Example5/Program.cs
@@ -0,0 +1,13 @@
+﻿int n1 = new Random().Next(1,1000);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+
+if (n1 % 2 == 0) 
+ {
+  Console.WriteLine("n1 - чётное число");
+ }
+  else
+ { 
+  Console.WriteLine("n1 - НЕчётное число");
+ }
