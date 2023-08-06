f3d1b6411136c7486b2bec7fa9e92fb6ffd32999
Danzille1 <chopper9751@mail.ru>
2023-08-06 23:01:55 +0500

    Sem 2 Task 10

---
+++ Examples/Example7/Program.cs
@@ -0,0 +1,13 @@
+﻿int n1 = new Random().Next(100,999);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+
+int n2 = (n1 /10 ) % 10;
+
+Console.Write("Вторая цифра = ");
+Console.WriteLine(n2);
+
+
+
+
