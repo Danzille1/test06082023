79ac6248db32bd0350f1528c1a7e3daebbb45927
Danzille1 <chopper9751@mail.ru>
2023-08-06 23:38:01 +0500

    Sem 2 Task 13

---
+++ Examples/Example8/Program.cs
@@ -0,0 +1,24 @@
+﻿int n1 = new Random().Next(1,999999);
+
+Console.Write("n1= ");
+Console.WriteLine(n1);
+
+if (n1 < 100)
+ {
+  Console.Write("Третьей цифры нет");     
+ }
+ else
+ {
+  int n3 = n1 /100;
+  if (n3 < 10)
+   {
+    Console.Write("Третья цифра = ");
+    Console.WriteLine(n3);
+   }
+   else
+    {
+     n3 = n3 % 10;  
+     Console.Write("Третья цифра = ");
+     Console.WriteLine(n3);
+    } 
+ }
