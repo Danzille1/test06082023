77ee7bc9cb9590a5eb12a2e7d56320a786518e03
Danzille1 <chopper9751@mail.ru>
2023-09-01 20:38:36 +0500

    Task 25 01092023

---
+++ Examples/Example11/Program.cs
@@ -0,0 +1,40 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int number = 51511;             
+int newnumber = 0;           
+int digits = 0;
+int oldnumber = number;
+
+Console.Write("number = "); 
+Console.WriteLine(number);
+
+while ( number > 0)
+ {
+  newnumber = newnumber * 10 + number % 10;   
+  number = number / 10;           
+  digits++;
+
+  if (digits > 5)
+  {
+   Console.WriteLine("more than 5 digits, false"); 
+   return;  
+  }
+ }
+
+if (digits < 5)
+ {
+   Console.WriteLine("less than 5 digits, false"); 
+   return;
+ }
+
+if (oldnumber == newnumber)
+ {
+  Console.WriteLine("palindrom !"); 
+ } 
+ else
+ {
+  Console.WriteLine("not a palindrom !");      
+ }
+
+Console.Write("new number = "); 
+Console.WriteLine(newnumber);
---
+++ Examples/Examples12/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+Console.Write("Input number = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write("Input degree = ");
+int b = int.Parse(Console.ReadLine());
+
+int res = 1;
+
+for (int i = 1; i<= b; i++)
+ {
+    res = res * a;
+ }
+
+Console.Write(a);
+Console.Write(",");
+Console.Write(b);
+Console.Write(" -> ");
+Console.WriteLine(res);
