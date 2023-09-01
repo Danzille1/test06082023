580a1668846207d98bbc9d31885976acd0f6360f
Danzille1 <chopper9751@mail.ru>
2023-09-01 20:44:59 +0500

    Task 27 01092023

---
+++ Examples/Examples13/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Input number = ");
+int a = int.Parse(Console.ReadLine());
+
+Console.Write(a);
+
+int sum = 0;
+
+while (a>0)
+{
+  sum = sum + (a % 10);
+  a = a /10;
+}
+
+Console.Write(" -> ");
+Console.WriteLine(sum);
