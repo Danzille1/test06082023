ed59664225eb2a91b308256acd7ba875d2f94b02
Danzille1 <chopper9751@mail.ru>
2023-09-05 00:26:39 +0500

    Task 34 04092023

---
+++ Examples/Example15/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+int[] arr = GetRandomArray(8);
+PrintArray(arr);
+
+CountEvenNumbers(arr);
+
+// Console.Write("res = "); 
+// Console.WriteLine(res);
+
+int[] GetRandomArray(int length)
+{
+    int[] result = new int[length];
+
+    for (int i = 0; i < length; i++)
+    {
+        result[i] = new Random().Next(100,999);
+    }
+    return result;
+}
+
+void CountEvenNumbers(int[] arr)
+{ 
+ int count  = 0;
+
+ for (int i = 0; i < arr.GetLength(0); i++)
+ { 
+  if (arr[i] % 2 == 0)
+  {
+    count++;
+  }
+ 
+ }
+ Console.WriteLine();
+ Console.WriteLine(count);
+}
+
+
+void PrintArray(int[] arr)
+{
+    foreach (int item in arr)
+    {
+        Console.Write(item+", ");
+    }
+}
+
