3515f79966e4ee8794e9ff923be52b9361bbd20d
Danzille1 <chopper9751@mail.ru>
2023-09-21 20:55:23 +0500

    Task 41-43 21092023

---
+++ Examples/Example18/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+Console.Write("Input M = ");
+string numStr = Console.ReadLine();
+int m = int.Parse(numStr);
+Console.WriteLine(m);
+
+int count = 0;
+for (int i = 1; i <= m; i++)
+{
+    Console.Write(i);
+    Console.Write(" Input Number = ");
+    string numStr1 = Console.ReadLine();
+    int number = int.Parse(numStr1);
+
+    if (number > 0) count++;
+}
+
+Console.Write("Amount of nubmers more than 0 = ");
+Console.WriteLine(count);
---
+++ Examples/Example19/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых,
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+Console.Write("Input b1 = ");
+string numStr1 = Console.ReadLine();
+float b1 = float.Parse(numStr1);
+
+Console.Write("Input k1 = ");
+string numStr2 = Console.ReadLine();
+float k1 = float.Parse(numStr2);
+
+Console.Write("Input b2 = ");
+string numStr3 = Console.ReadLine();
+float b2 = float.Parse(numStr3);
+
+Console.Write("Input k2 = ");
+string numStr4 = Console.ReadLine();
+float k2= float.Parse(numStr4);
+
+float x = (b2 - b1) / (k1 - k2);
+float y = k1 * x + b1;
+
+Console.Write("Crossing point coordinates: x= ");
+Console.Write(x);
+Console.Write(" y= ");
+Console.WriteLine(y);
